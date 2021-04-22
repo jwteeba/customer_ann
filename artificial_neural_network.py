e87694768e662fc104cc535bd957049ad6a9bd83
Jah-Wilson Teeba <teebawilson91@gmail.com>
2021-04-22 16:05:49 -0400

    enhanced model

--- artificial_neural_network.py
+++ artificial_neural_network.py
@@ -1,66 +1,50 @@
 # Artificial Neural Network
 
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
-from sklearn.metrics import confusion_matrix
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.compose import ColumnTransformer
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
 
 # Import the dataset
-dataset = pd.read_csv('data/Churn_Modelling.csv')
-X = dataset.iloc[:, 3:13].values # X is the independent variable
-y = dataset.iloc[:, 13].values # y is the dependent variable
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, -1].values
 
 # Encode categorical data
-labelencoder_X_1 = LabelEncoder()
-X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
-labelencoder_X_2 = LabelEncoder()
-X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-ct = ColumnTransformer([('one_hot_encoder', OneHotEncoder(categories='auto'), [1])], remainder='passthrough')
-X = ct.fit_transform(X)
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+X[:, 2] = le.fit_transform(X[:, 2])
 
-#onehotencoder = OneHotEncoder(categorical_features = [1])
-#X = onehotencoder.fit_transform(X).toarray()
-X = X[:, 1:]
+# One Hot Encoding the "Geography" column
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
 
 
 # Split the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
+# Apply Feature Scaling
+from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Initialising the ANN
-classifier = Sequential()
+# Create ANN Model
+ann_model = Sequential()
+ann_model.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann_model.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann_model.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann_model.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
 
-# Adding the input layer and the first hidden layer
-classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))
 
-# Adding the second hidden layer
-classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
+# Compile ANN Model
+ann_model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-# Adding the output layer
-classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+# Train model
+history = ann_model.fit(X_train, y_train, batch_size = 32, epochs = 100)
 
-# Compiling the ANN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-
-# Fitting the ANN to the Training set
-classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
-
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-y_pred = (y_pred > 0.5)
-
-# Making the Confusion Matrix
-cm = confusion_matrix(y_test, y_pred)
-
-#accuracy = (cm[0][0] + cm[1][1]) / len(X_test)
+# ann_model.save('customer_management.h5', overwrite=True)
